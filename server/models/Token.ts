cb042e3dfcd76e8063f0e6e7d4b9e635f8d0c0bd
Andranik Antonyan <andantonyan@gmail.com>
2017-10-19 18:30:42 +0400

    feat(server:models): Token

---
+++ server/models/Token.ts
@@ -0,0 +1,52 @@
+import { BelongsTo, Column, CreatedAt, DataType, DeletedAt, ForeignKey, Model, Table, UpdatedAt } from 'sequelize-typescript';
+import { User } from './User';
+import { TokenType } from '../../common/enums/index';
+
+@Table({
+  tableName: 'token',
+  underscored: true,
+  paranoid: true,
+  timestamps: true
+})
+export class Token extends Model<Token> {
+  /**
+   * Fields
+   */
+  @Column(DataType.DATE)
+  expiration: Date;
+
+  @Column(DataType.DATE)
+  used: Date;
+
+  @Column({
+    type: DataType.ENUM(TokenType.AUTH, TokenType.AUTH_REMEMBER_ME, TokenType.PASSWORD_RESET, TokenType.RESET_EMAIL)
+  })
+  type: TokenType;
+
+  @Column(DataType.JSON)
+  metadata: any;
+  /**
+   * Associations
+   */
+  @ForeignKey(() => User)
+  @Column({field: 'user_id'})
+  userId: number;
+
+  @BelongsTo(() => User)
+  user: User;
+
+  /**
+   * Default
+   */
+  /**
+   * Defaults
+   */
+  @CreatedAt
+  created: Date;
+
+  @UpdatedAt
+  updated: Date;
+
+  @DeletedAt
+  deleted: Date;
+}
