d7702a3f77d7a2f3a098b33b54a3475b3db20d42
Andranik Antonyan <andantonyan@gmail.com>
2017-11-19 06:19:17 +0400

    fix

--- src/app/core/shared/components/sidenav/sidenav.component.ts
+++ src/app/core/shared/components/sidenav/sidenav.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
+import { UserDto } from '../../../../../../common/models/user.model';
+
 interface ISidenavComponent {
   onClick($event: Event): void;
 }
@@ -13,8 +15,11 @@ interface ISidenavComponent {
 export class SidenavComponent implements ISidenavComponent, OnInit {
   @Input() open = false;
   @Input() mode: string;
+  @Input() user: UserDto;
+  @Input() selectedUser: UserDto;
   @Output() onItemActivate = new EventEmitter();
   @Output() onClose = new EventEmitter();
+  @Output() onSelectedUserChange = new EventEmitter<number>();
   isHomeActive;
   constructor(private router: Router) {
   }
