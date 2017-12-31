f13592b72eb193a52ab785f4be2c1cefd2f44faf
Andranik Antonyan <andantonyan@gmail.com>
2017-12-31 08:42:25 +0400

    feat(auth): update styles

---
+++ src/app/core/shared/actions/index.ts
@@ -0,0 +1,11 @@
+import { Action } from '@ngrx/store';
+
+export const CLEAR = '[Main] Clear';
+
+export class Clear implements Action {
+  readonly type = CLEAR;
+
+  constructor(public payload: string[]) {}
+}
+
+export type Actions = Clear;
